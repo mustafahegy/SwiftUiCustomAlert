3308e6c7aed366c6789950574a465e732d2bc588
Madar iOS <madar.developer.iosapps@gmail.com>
2023-10-30 22:51:44 +0200

    add background shadow

--- Classes/AlertView.swift
+++ Classes/AlertView.swift
@@ -10,46 +10,58 @@ import SwiftUI
 public struct AlertView: View {
     public let title: String
     public let description: String
+    public let confirmButtonLabel: String
+    public let cancelButtonLabel: String
     public let confirmAction: () -> Void
     public let cancelAction: () -> Void
     
-    public init(title: String, description: String, confirmAction: @escaping () -> Void, cancelAction: @escaping () -> Void) {
+    public init(title: String, description: String, confirmButtonLabel: String, cancelButtonLabel: String, confirmAction: @escaping () -> Void, cancelAction: @escaping () -> Void) {
         self.title = title
         self.description = description
+        self.confirmButtonLabel = confirmButtonLabel
+        self.cancelButtonLabel = cancelButtonLabel
         self.confirmAction = confirmAction
         self.cancelAction = cancelAction
     }
     
     @available(iOS 13.0.0, *)
     public var body: some View {
-        VStack {
-            Text(title)
-                .font(.title)
-            Text(description)
-                .font(.body)
-                .padding()
-            
-            HStack {
-                Button("Cancel") {
-                    cancelAction()
-                }
-                .padding()
-                .background(Color.red)
-                .foregroundColor(.white)
-                .cornerRadius(8)
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 10) // Rounded background
+                .fill(Color.white)
+                .shadow(radius: 5)
+            VStack {
+                Text(title)
+                    .font(.title)
+                Text(description)
+                    .font(.body)
+                    .padding()
                 
-                Button("Confirm") {
-                    confirmAction()
+                HStack {
+                    Button(cancelButtonLabel) {
+                        cancelAction()
+                    }
+                    .frame(height: 40)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    
+                    Button(confirmButtonLabel) {
+                        confirmAction()
+                    }
+                    .frame(height: 40)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
                 }
-                .padding()
-                .background(Color.green)
-                .foregroundColor(.white)
-                .cornerRadius(8)
             }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(12)
         }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(12)
     }
     
 }
